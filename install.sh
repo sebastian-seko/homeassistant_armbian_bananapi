ec881032826895ddf43860bf5cba1861adf36cd4
sebastian-seko <ssebastian.kowalczyk@gmail.com>
2021-04-03 12:06:27 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y python3-dev python3-pip libffi-dev libssl-dev
+sudo apt-get install -y libjpeg-dev
+apt-get install -y python-dev
+pip3 install pillow==8.1.1
+pip3 install homeassistant
+hass --open-ui
